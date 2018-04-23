b612abcebfd69e142521bc34952f962e8ba002e0
Tomas Meszaros <exo@tty.sk>
2018-04-23 17:48:27 +0200

    fixed generation

--- postgres/generate.py
+++ postgres/generate.py
@@ -47,6 +47,10 @@ def advance_time(by_hour=1):
 def get_db_time():
     return """%d-%d-%d %d:%d""" % (TIME["year"], TIME["month"], TIME["day"], TIME["hour"], TIME["minute"])
 
+################################################################################
+## category                                                                   ##
+################################################################################
+
 def insert_category():
     category_name = utils.get_random_string()
     utils.execute("""INSERT INTO category (name) VALUES ('{CATEGORY_NAME}');""".format(
@@ -59,6 +63,10 @@ def update_category():
     utils.execute("""update category set name = '{RAND_NAME}' where id = {RAND_ID};""".format(
         RAND_NAME=rand_name, RAND_ID=rand_id))
     advance_time()
+
+################################################################################
+## product                                                                    ##
+################################################################################
 
 def insert_product():
     product_name = utils.get_random_string()
@@ -76,6 +84,10 @@ def update_product():
         PRODUCT_COST=product_cost, RAND_ID=rand_id))
     advance_time()
 
+################################################################################
+## customer                                                                   ##
+################################################################################
+
 def insert_customer():
     customr_name = utils.get_random_string()
     customer_email = utils.get_random_string()
@@ -92,34 +104,75 @@ def update_customer():
         CUSTOMER_ACTIVE=customer_active, RAND_ID=rand_id))
     advance_time()
 
-def insert_category_product():
+################################################################################
+## category_product                                                           ##
+################################################################################
+
+_category_product_pairs = []
+def _get_pair_category_product():
     category_num_ids = utils.count_rows("category")
     product_num_ids = utils.count_rows("product")
     category_product_category_id = utils.get_random_number(1, category_num_ids)
     category_product_product_id = utils.get_random_number(1, product_num_ids)
+    return (category_product_category_id, category_product_product_id)
+
+def insert_category_product():
+    found_good_pair = False
+    while not found_good_pair:
+        pair = _get_pair_category_product()
+        if pair not in _category_product_pairs:
+            _category_product_pairs.append(pair)
+            found_good_pair = True
+            break
+    if not found_good_pair:
+        return
+    category_product_category_id = pair[0]
+    category_product_product_id = pair[1]
+
     utils.execute("""INSERT INTO category_product (category_id, product_id) VALUES ({CATEGORY_PRODUCT_CATEGORY_ID}, {CATEGORY_PRODUCT_PRODUCT_ID});""".format(
         CATEGORY_PRODUCT_CATEGORY_ID=category_product_category_id, CATEGORY_PRODUCT_PRODUCT_ID=category_product_product_id))
     advance_time()
 
-def update_category_product():
+def NOT_USED_update_category_product():
     category_num_ids = utils.count_rows("category")
     product_num_ids = utils.count_rows("product")
     category_product_category_id = utils.get_random_number(1, category_num_ids)
     category_product_product_id = utils.get_random_number(1, product_num_ids)
+
     utils.execute("""update category_product set category_id = {CATEGORY_PRODUCT_CATEGORY_ID} where product_id = {CATEGORY_PRODUCT_PRODUCT_ID};""".format(
         CATEGORY_PRODUCT_CATEGORY_ID=category_product_category_id, CATEGORY_PRODUCT_PRODUCT_ID=category_product_product_id))
     advance_time()
 
-def insert_purchase():
+################################################################################
+## purchase                                                                   ##
+################################################################################
+
+_customer_product_pairs = []
+def _get_pair_customer_product():
     customer_num_ids = utils.count_rows("customer")
     product_num_ids = utils.count_rows("product")
     purchase_customer_id = utils.get_random_number(1, customer_num_ids)
     purchase_product_id = utils.get_random_number(1, product_num_ids)
+    return (purchase_customer_id, purchase_product_id)
+
+def insert_purchase():
+    found_good_pair = False
+    while not found_good_pair:
+        pair = _get_pair_customer_product()
+        if pair not in _customer_product_pairs:
+            _customer_product_pairs.append(pair)
+            found_good_pair = True
+            break
+    if not found_good_pair:
+        return
+    purchase_customer_id = pair[0]
+    purchase_product_id = pair[1]
+
     utils.execute("""INSERT INTO purchase (date_time, customer_id, product_id) VALUES ('{DB_TIME}', {PURCHASE_CUSTOMER_ID}, {PURCHASE_PRODUCT_ID});""".format(
         DB_TIME=get_db_time(), PURCHASE_CUSTOMER_ID=purchase_customer_id, PURCHASE_PRODUCT_ID=purchase_product_id))
     advance_time()
 
-def update_purchase():
+def NOT_USED_update_purchase():
     customer_num_ids = utils.count_rows("customer")
     product_num_ids = utils.count_rows("product")
     purchase_customer_id = utils.get_random_number(1, customer_num_ids)
@@ -129,19 +182,30 @@ def update_purchase():
     advance_time()
 
 
+################################################################################
+## GENERATION                                                                 ##
+################################################################################
+
 ## list of db modifications
 functions = [insert_category, update_category,
              insert_product, update_product,
              insert_customer, update_customer,
-             insert_category_product, update_category_product,
-             insert_purchase, update_purchase]
+             insert_category_product,
+             insert_purchase,
+             #insert_category_product, update_category_product,
+             #insert_purchase, update_purchase]
+             ]
 
 ## initial inserts
-insert_category()
-insert_product()
-insert_customer()
-insert_category_product()
-insert_purchase()
+def _init():
+    insert_category()
+    insert_product()
+    insert_customer()
+for _ in range(20):
+    _init()
+
+#insert_category_product()
+#insert_purchase()
 
 ## db life simulation
 for _ in range(DB_CHANGES):
